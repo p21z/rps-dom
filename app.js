73950d5071422d6245ee2e7e65012e46edf597d4
p21z <prim@hackthenorth.ph>
2021-09-23 22:01:23 +0800

    added UI

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let choices=['rock', 'paper', 'scissors'];
+let playerScore = 0;
+let computerScore = 0;
+let roundcount = 0;
 
 computerPlay = () => 
 {
@@ -13,49 +16,50 @@ playRound  = (computerChoice, playerChoice) =>
     {
         if (playerChoice == "rock")
         {
-            console.log('Tie')
+            result = "Tie"
         } else if (playerChoice == "paper")
         {
-            console.log('You win!')
+            result = "YOu Win!"
             return playerScore++
         } else{
-            console.log('You lose!')
+            result = "You Lose!"
             return computerScore++
         }
     } else if (computerChoice == "paper")
     {
         if (playerChoice == "rock")
         {
-            console.log('You lose!')
+            result = " You Lose!"
             return computerScore++
         } else if (playerChoice == "paper")
         {
+            result = "Tie"
             console.log('Tie')
         } else {
-            console.log('You win!')
+            result = "YOu Win!"
             return playerScore++
         }
     } else
     {
         if (playerChoice == "rock")
         {
-            console.log('You Win!')
+            result = "YOu Win!"
             return playerScore++
         } else if (playerChoice == "paper")
         {
-            console.log('You lose!')
+            result = " You Lose!"
             return computerScore++
         } else {
-            console.log('Tie!')
+            result = "Tie"
         }
     }
     //      
 }
 
-playerPlay = () => 
+playerPlay = (e) => 
 {
-    input=prompt('Make your choice: (rock, paper, scissors)')
-    playerChoice = input.toLowerCase()
+    // input=prompt('Make your choice: (rock, paper, scissors)')
+    playerChoice = e.toLowerCase()
     if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors")
     {
         console.log('Player choice: ', playerChoice);
@@ -68,21 +72,40 @@ playerPlay = () =>
     }
 }
 
-game = () =>
+display = () =>
 {
-    playerScore = 0;
-    computerScore = 0;
+    playerScoreView = document.querySelector('#playerScoreView');
+    playerScoreView.innerHTML = playerScore;
+    
+    compScoreView = document.querySelector('#compScoreView');
+    compScoreView.innerHTML = computerScore;
 
-    for (let i=0; i<5; i++)
-    {
-        console.log('Round ', i+1)
-        playerSelection = playerPlay()
-        console.log('playerScore: ', playerScore)
-        console.log('computerScore: ', computerScore)
-    }
+    resultView = document.querySelector('.result');
+    resultView.innerHTML = result.toUpperCase();
+
 }
 
-game()
+game = (e) =>
+{
+    roundcount++;
+    console.log('Round ',roundcount)
+    playerSelection =  e.target.getAttribute('data-attribute');
+    playerPlay(playerSelection)
+    console.log('playerScore: ', playerScore)
+    console.log('computerScore: ', computerScore)
+    display()
+
+}
+
+
+const btn = document.querySelectorAll('button');
+
+btn.forEach(element => {
+    element.addEventListener('click', game);
+});
+
+
+// game()
 
 
 
